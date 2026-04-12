0a7835a78c9c689e06f2e00200b982239d3d0094
REDA-dnt <habchi.mohamedreda@ensam-casa.ma>
2026-04-12 14:31:09 +0100

    Add HasFactory trait to Offre model

--- app/Models/Offre.php
+++ app/Models/Offre.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offre extends Model
 {
+    use HasFactory;
     protected $fillable = ['user_id', 'titre', 'description', 'localisation', 'type', 'actif'];
 
     public function recruteur()     { return $this->belongsTo(User::class, 'user_id'); }
