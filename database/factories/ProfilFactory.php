80824a6376198b36076c38459e96ac4445d03287
REDA-dnt <habchi.mohamedreda@ensam-casa.ma>
2026-04-12 14:19:17 +0100

    Refactor ProfilFactory and enhance definition
    Removed unused imports and docblocks, and added fields to the factory definition.

--- database/factories/ProfilFactory.php
+++ database/factories/ProfilFactory.php
@@ -2,23 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\Profil;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends Factory<Profil>
- */
 class ProfilFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'titre'        => $this->faker->jobTitle(),
+            'bio'          => $this->faker->paragraph(),
+            'localisation' => $this->faker->city(),
+            'disponible'   => $this->faker->boolean(70),
         ];
     }
 }
