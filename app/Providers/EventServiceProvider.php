794d36a435f47caa045e936010506a623f140786
REDA-dnt <habchi.mohamedreda@ensam-casa.ma>
2026-04-16 23:33:45 +0100

    Add files via upload

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    protected $listen = [
+        \App\Events\CandidatureDeposee::class => [
+            \App\Listeners\LogCandidatureDeposee::class,
+        ],
+
+        \App\Events\StatutCandidatureMis::class => [
+            \App\Listeners\LogStatutCandidatureMis::class,
+        ],
+    ];
+
+    public function boot(): void
+    {
+        //
+    }
+}
