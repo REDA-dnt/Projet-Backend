9be5bceac4eaebff3b1e10cc831f61a6fe775651
REDA-dnt <habchi.mohamedreda@ensam-casa.ma>
2026-04-16 22:07:07 +0100

    Update CandidatureSeeder.php

--- database/seeders/CandidatureSeeder.php
+++ database/seeders/CandidatureSeeder.php
@@ -11,7 +11,21 @@ class CandidatureSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        //
+{
+    $profils = Profil::all();
+    $offres = Offre::where('actif', true)->get();
+
+    foreach ($profils as $profil) {
+        $offresSelectionnees = $offres->random(2);
+        
+        foreach ($offresSelectionnees as $offre) {
+            Candidature::create([
+                'offre_id' => $offre->id,
+                'profil_id' => $profil->id,
+                'message' => 'Je suis très intéressé par ce poste.',
+                'statut' => 'en_attente'
+            ]);
+        }
     }
+}
 }
